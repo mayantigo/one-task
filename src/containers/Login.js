6924e1d5f0dc37f9d489e09a95489c6e04cca551
francisco.gonzalez <francisco.gonzalez@barceloviajes.com>
2017-11-23 16:15:37 +0100

    Adding flow to login.

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,8 +1,10 @@
+// @flow
+
 import { connect } from 'react-redux';
 import Login from '../screens/login';
 
 const mapDispatchToProps = dispatch => ({
-  login(user, password) {
+  login(user: string, password: string) {
     dispatch(user + password);
   },
 });
